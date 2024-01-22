c2018c724e55392ed26562ef01e844319437f4ee
opilane <opilane@HP-CZC2349HTN>
2024-01-22 14:08:01 +0200

    Add project files.

---
+++ Books/Books/Views/BookPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Books.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Books.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookPage : ContentPage
+    {
+        public BookViewModel ViewModel { get; private set; }
+        public BookPage(BookViewModel vm)
+        {
+            InitializeComponent();
+            ViewModel = vm;
+            this.BindingContext = ViewModel;
+        }
+    }
+}
---
+++ Books/Books/Views/BooksListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Books.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Books.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BooksListPage : ContentPage
+    {
+        public BooksListPage()
+        {
+            InitializeComponent();
+            BindingContext = new BooksListViewModel()
+            {
+                Navigation = this.Navigation
+            };
+        }
+    }
+}
